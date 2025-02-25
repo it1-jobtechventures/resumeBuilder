2359de5215b9c3d1e6af249f8689a608643b3373
it <it1.jobtechventure.com>
2025-02-25 16:22:29 +0530

    add new field

--- frontend/src/components/GeneralInfo.jsx
+++ frontend/src/components/GeneralInfo.jsx
@@ -7,29 +7,40 @@ const GeneralInfo = ({nextStep}) => {
         <h2 className="text-2xl font-bold mb-4">Basic Information</h2>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700">Full Name</label>
-            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your full name" />
+            <label className="block text-gray-700">First Name<span className='text-red-700 pl-0.5'>*</span></label>
+            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your first name"required />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
-            <input type="email" className="w-full p-2 border rounded-md" placeholder="Enter your email" />
+            <label className="block text-gray-700">Last Name<span className='text-red-700 pl-0.5'>*</span></label>
+            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your Last name" required/>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700">Phone Number</label>
-            <input type="tel" className="w-full p-2 border rounded-md" placeholder="Enter your phone number" />
-          </div>  
-          <div className="mb-4">
-            <label className="block text-gray-700">Address</label>
-            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your address" />
+            <label className="block text-gray-700">Email<span className='text-red-700 pl-0.5'>*</span></label>
+            <input type="email" className="w-full p-2 border rounded-md" placeholder="Enter your email" required/>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700">Job Role</label>
-            <select className="w-full p-2 border rounded-md">
-              <option>Select your role</option>
-              <option>Frontend Developer</option>
-              <option>Backend Developer</option>
-              <option>Full Stack Developer</option>
-            </select>
+            <label className="block text-gray-700">Phone Number<span className='text-red-700 pl-0.5'>*</span></label>
+            <input type="tel" className="w-full p-2 border rounded-md" placeholder="Enter your phone number" required/>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">City</label>
+            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your City" />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Country<span className='text-red-700 pl-0.5'>*</span></label>
+            <input type="text" className="w-full p-2 border rounded-md" placeholder="Enter your Country" required/>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Pincode</label>
+            <input type="Number" className="w-full p-2 border rounded-md" placeholder="Enter your Pincode" />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">LinkedIn Url</label>
+            <input type="url" className="w-full p-2 border rounded-md" placeholder="Enter your linkedin url" />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Portfolio</label>
+            <input type="url" className="w-full p-2 border rounded-md" placeholder="Enter your Portfolio" />
           </div>
           <div className="flex justify-end">
             <button type="button" onClick={nextStep} className="bg-[linear-gradient(90deg,_hsla(133,_68%,_60%,_1)_0%,_hsla(205,_97%,_42%,_1)_100%)] cursor-pointer text-white px-4 py-2 rounded-md hover:bg-[linear-gradient(90deg,_hsla(205,_97%,_42%,_1)_0%,_hsla(133,_68%,_60%,_1)_100%)]">
