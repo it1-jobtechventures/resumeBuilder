2681abe94085766951321e4b6046f2a3b4e0e0cb
it <it1.jobtechventure.com>
2025-03-01 10:30:26 +0530

    addlink

--- frontend/src/components/SocialMediaSection.jsx
+++ frontend/src/components/SocialMediaSection.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { BsInstagram } from "react-icons/bs";
 import { RiFacebookBoxFill } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaPinterest } from "react-icons/fa6"
 
 const SocialMediaSection = () => {
   return (
     <>
         <main className='border  m-1.5'>
             <div className='flex flex-col gap-6 p-10'>
-            <div className='flex items-center gap-4 border h-12 p-2'>
-                <BsInstagram className='text-2xl'/>
-                <input type='url' placeholder='Enter your Instagram profile link' className='outline-none w-full'/>
+                <div className='flex items-center gap-4 border h-12 p-2'>
+                    <BsInstagram className='text-2xl'/>
+                    <input type='url' placeholder='Enter your Instagram profile link' className='outline-none w-full'/>
+                </div>
+                <div className='flex items-center gap-4 border h-12 p-2'>
+                    <RiFacebookBoxFill className='text-2xl'/>
+                    <input type='url' placeholder='Enter your facebook profile link' className='outline-none w-full'/>
+                </div>
+                <div className='flex items-center gap-4 border h-12 p-2'>
+                    <FaWhatsapp className='text-2xl'/>
+                    <input type='url' placeholder="Enter your What's app profile link" className='outline-none w-full'/>
+                </div>
+                <div className='flex items-center gap-4 border h-12 p-2'>
+                    <RiTwitterXFill className='text-2xl'/>
+                    <input type='url' placeholder='Enter your X profile link' className='outline-none w-full'/>
+                </div>
+                <div className='flex items-center gap-4 border h-12 p-2'>
+                    <FaPinterest className='text-2xl'/>
+                    <input type='url' placeholder='Enter your Pinterest profile link' className='outline-none w-full'/>
+                </div>
             </div>
-            <div className='flex items-center gap-4 border h-12 p-2'>
-                <RiFacebookBoxFill className='text-2xl'/>
-                <input type='url' placeholder='Enter your facebook profile link' className='outline-none w-full'/>
-            </div>
-            </div>
-
         </main>
     </>
   )
