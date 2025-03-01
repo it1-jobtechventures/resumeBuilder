a6951902656755df4fd2e92d3342cdf5b55d782e
it <it1.jobtechventure.com>
2025-03-01 16:23:50 +0530

    created dummy template data

---
+++ frontend/src/assets/templateData.js
@@ -0,0 +1,23 @@
+// src/data/templatesData.js
+const templatesData = [
+    {
+      id: 1,
+      name: "Classic",
+      image: "https://s3.eu-west-2.amazonaws.com/resumedone-eu-west-2-staging/L6DRzMH59L-photo.png", // Update path accordingly
+      description: "A timeless and professional template.",
+    },
+    {
+      id: 2,
+      name: "Modern",
+      image: "https://images.highfile.com/wp-content/uploads/2024/04/Minimalist-Resume-Template-Word-Google-Docs.png?strip=all&lossy=1&quality=90&webp=90&sharp=1&ssl=1",
+      description: "A sleek and contemporary design.",
+    },
+    {
+      id: 3,
+      name: "Creative",
+      image: "https://d25zcttzf44i59.cloudfront.net/official-resume-template.png",
+      description: "A bold and unique layout for creatives.",
+    },
+  ];
+  
+export default templatesData;  
