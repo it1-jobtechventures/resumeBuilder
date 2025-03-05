bf28dbc2818650f6c60b7d9a9521d7980f9053ce
it <it1.jobtechventure.com>
2025-03-05 11:45:11 +0530

    mounted context api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import { FormProvider } from './context/FormContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FormProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FormProvider>
   </StrictMode>,
 )
