bc714cc6db15debad9ab00e4d627b5bf1404b275
it <it1.jobtechventure.com>
2025-03-07 14:02:04 +0530

    created components and routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Router, Routes } from 'react-router-dom'
 import './App.css'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
+      <div  div className="flex">
+        <Sidebar/>
+        <div className="flex-1 p-6">
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
