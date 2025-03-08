98b6285933c19701b51415ac6efd0b1e49829a42
it <it1.jobtechventure.com>
2025-03-08 10:13:47 +0530

    add routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,21 @@
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/Sidebar'
 import Dashboard from './pages/Dashboard'
+import Navbar from './components/Navbar'
+import Template from './components/Template'
 
 function App() {
 
   return (
     <>
       <div  div className="flex">
+        <Navbar/>
         <Sidebar/>
         <div className="flex-1 p-6">
           <Routes>
             <Route path='/' element={<Dashboard/>}/>
+            <Route path='/template' element={<Template/>}/>
           </Routes>
         </div>
       </div>
