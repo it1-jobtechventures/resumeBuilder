f7eafc22dc402917d0ce9c260acf95516e89a0b5
it <it1.jobtechventure.com>
2025-03-08 10:09:25 +0530

    created navbar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="h-16 bg-amber-300 w-full flex items-center px-4 fixed top-0 left-0 z-10 shadow-lg">
+        <h1 className="text-xl font-semibold">ACHAL</h1>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
