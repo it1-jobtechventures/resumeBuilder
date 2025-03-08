51fa179f08cf35197e296f72ceab55d41f32a0f2
it <it1.jobtechventure.com>
2025-03-08 10:10:12 +0530

    sidebar for navigation

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <>
+        <div className="w-64 h-screen bg-gray-900 text-white p-4 flex flex-col">
+            <nav className="flex flex-col space-y-4">
+                <Link to="/" className="hover:bg-gray-700 p-2 rounded">
+                    Dashboard
+                </Link>
+                <Link to="/template" className="hover:bg-gray-700 p-2 rounded">
+                    Template
+                </Link>
+            </nav>
+        </div>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
