0d8ed6992332b5fbacbb3da42b81ad8268a442d9
it <it1.jobtechventure.com>
2025-03-17 13:25:13 +0530

    change image to previeImage

--- frontend/src/components/TemplateCard.jsx
+++ frontend/src/components/TemplateCard.jsx
@@ -5,7 +5,7 @@ const TemplateCard = ({ template, onSelect }) => {
     <>
       <main>
         <div className="border p-4 rounded-md cursor-pointer" onClick={() => onSelect(template)}>
-          <img src={template.image} alt={template.name} className="w-full h-48 object-cover rounded-md" />
+          <img src={template.previewImage} alt={template.name} className="w-full h-48 object-cover rounded-md" />
           <h3 className="text-lg font-semibold mt-2">{template.name}</h3>
         </div>
       </main>
