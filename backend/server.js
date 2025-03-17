86e91210d89bbb676ec9053e963e988b144d7b81
it <it1.jobtechventure.com>
2025-03-17 13:20:24 +0530

    add url endcoded

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import templateRouter from './routes/templateRoutes.js';
 
 dotenv.config();
 
@@ -10,9 +11,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended:false }));
 
 // Database Connection
 connectDB();
+
+//Routes
+app.use('/api/template' ,templateRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
