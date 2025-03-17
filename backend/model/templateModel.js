de628cfeb601fb5156d35d4dda7681ae4b0b8ae7
it <it1.jobtechventure.com>
2025-03-17 16:03:55 +0530

    update field namw

--- backend/model/templateModel.js
+++ backend/model/templateModel.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const templateSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    htmlFile: { type: String },
-    cssFile: { type: String },
-    jsFile: { type: String },
+    htmlContent: { type: String },
+    cssContent: { type: String },
+    jsContent: { type: String },
     previewImage: { type: String }, 
   },
   { timestamps: true }
