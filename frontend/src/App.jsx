db4b1aa3958edb871a41bd94fda422cb0b445148
it <it1.jobtechventure.com>
2025-03-18 15:45:13 +0530

    import toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,14 @@ import Login from './components/Login'
 import ResumePage from './pages/ResumePage'
 import TemplatePage from './pages/TemplatePage'
 import ResumeReview from './components/ResumeReview'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
  
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<Login/>}/>
