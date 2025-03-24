98333d2763303cd64316656dcd188a717d60551d
it <it1.jobtechventure.com>
2025-03-24 17:51:49 +0530

    add routes for user , create a function  to check user is exits or not

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import templateRouter from './routes/templateRoutes.js';
+import authRouter from './routes/authRoutes.js';
+import userModel from './model/userModel.js';
 
 dotenv.config();
 
 const app = express();
+const allowedOrigins = ['http://localhost:5173']
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
+
+
+const logUsers = async () => {
+    try {
+        const users = await userModel.find();
+        console.log("Users in Database:", users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+    }
+};
 
 // Database Connection
-connectDB();
+connectDB().then(() => logUsers());
 
 //Routes
 app.use('/api/template' ,templateRouter)
+app.use('/api/auth' ,authRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
