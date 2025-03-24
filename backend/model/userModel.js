603f3ee59731fd7951ea89f073c12e8b1504b9a2
it <it1.jobtechventure.com>
2025-03-24 17:50:06 +0530

    users field model

---
+++ backend/model/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {type: String,required: [true, "Name is required"],},
+    email: {type: String,required: [true, "Email is required"],unique: true},
+    password: {type: String,required: [true, "Password is required"],},
+    resetOtp: {type: String,default: ''},
+    resetOtpExpireAt: {type: Number,default: 0 },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
