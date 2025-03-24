48957a5766a31b2d1f7a34497dffc88dcc9fc391
it <it1.jobtechventure.com>
2025-03-24 17:38:27 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import Login from './components/Login'
+import Login from './pages/Login'
 import ResumePage from './pages/ResumePage'
 import TemplatePage from './pages/TemplatePage'
 import ResumeReview from './components/ResumeReview'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
  
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>} />
         <Route path='/createResume' element={<ResumePage/>}/>
         <Route path='/templates' element={<TemplatePage/>}/>
         <Route path='/resume-review' element={<ResumeReview/>}/>
