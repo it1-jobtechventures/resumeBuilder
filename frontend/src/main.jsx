285d030a90b255b939e14448f907ec19eef0e0d8
it <it1.jobtechventure.com>
2025-03-24 17:39:21 +0530

    wrap app contexr

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { FormProvider } from './context/FormContext'
+import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FormProvider>
       <BrowserRouter>
+      <AppContextProvider>
         <App />
+      </AppContextProvider>
       </BrowserRouter>
     </FormProvider>
   </StrictMode>,
