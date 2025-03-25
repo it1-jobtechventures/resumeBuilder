6855d051ce05904fe00c5d5f87ac054311e675fa
it <it1.jobtechventure.com>
2025-03-25 17:08:27 +0530

    save logged in state in local storage and fetch when site is to check initially user is logged in or not

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
     const backendUrl = "http://localhost:5000";
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Initialize state from localStorage
+    const [isLoggedIn, setIsLoggedIn] = useState(() => {
+        return localStorage.getItem("isLoggedIn") === "true";
+    });
+
+    useEffect(() => {
+        localStorage.setItem("isLoggedIn", isLoggedIn);
+    }, [isLoggedIn]);
 
     const value = {
         backendUrl,
-        isLoggedIn, setIsLoggedIn,
+        isLoggedIn, 
+        setIsLoggedIn,
     }
 
     return (
