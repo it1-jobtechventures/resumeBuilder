2adcb025416f9db922544771e68a762520a14090
it <it1.jobtechventure.com>
2025-03-25 15:14:42 +0530

    update function and add confdition

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -1,28 +1,53 @@
+// import jwt from "jsonwebtoken";
+
+// const userAuth = async (req, res, next) => {
+//     const {token} = req.cookies;
+
+//     if(!token) {
+//         return res.json({status:false, message: "Not authorised. Login again"})
+//     }
+
+//     try {
+//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+//         if(tokenDecode.id) {
+//             req.body.userId = tokenDecode.id
+//         } else {
+//             return res.json({
+//                 success: false,
+//                 message: 'Not Authorised. Login Again'
+//             });
+//         }
+
+//         next();
+//     } catch (error) {
+//         res.json({success: false, message: error.message})
+//     }
+// }
+
+// export default userAuth;
+
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
-    const {token} = req.cookies;
+    const { token } = req.cookies;
 
-    if(!token) {
-        return res.json({status:false, message: "Not authorised. Login again"})
+    if (!token) {
+        return res.status(401).json({ status: false, message: "Not authorized. Login again" });
     }
 
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
-        if(tokenDecode.id) {
-            req.body.userId = tokenDecode.id
+        if (tokenDecode && tokenDecode.id) {
+            req.user = { id: tokenDecode.id }; // Attach user data to req
+            next();
         } else {
-            return res.json({
-                success: false,
-                message: 'Not Authorised. Login Again'
-            });
+            return res.status(401).json({ success: false, message: "Not Authorized. Login Again" });
         }
-
-        next();
     } catch (error) {
-        res.json({success: false, message: error.message})
+        return res.status(401).json({ success: false, message: "Invalid Token: " + error.message });
     }
-}
+};
 
 export default userAuth;
