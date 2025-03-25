12465a1019c142dbc4e0735fc17f7204060ca53f
it <it1.jobtechventure.com>
2025-03-25 15:13:50 +0530

    add route for user profile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { isAuthenticated, login, logout, resetPassword, sendResetOtp, signup } from "../controllers/authController.js";
+import { getUserProfile, isAuthenticated, login, logout, resetPassword, sendResetOtp, signup } from "../controllers/authController.js";
 import userAuth from "../middleware/userAuth.js";
 
 const authRouter = Router();
@@ -10,5 +10,6 @@ authRouter.post("/logout", logout);
 authRouter.post("/is-auth", userAuth, isAuthenticated)
 authRouter.post("/send-reset-otp", sendResetOtp);
 authRouter.post("/reset-password", resetPassword);
+authRouter.get("/profile", userAuth, getUserProfile);
 
 export default authRouter;
