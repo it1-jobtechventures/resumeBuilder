6d19f6dc9bb28fb23fecb5b96633cc10284cf786
it <it1.jobtechventure.com>
2025-03-28 15:42:47 +0530

    add json for languag

---
+++ frontend/src/assets/language.js
@@ -0,0 +1,408 @@
+const language = [
+    {
+        "id": 1,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "language": "Hindi"
+    },
+    {
+        "id": 3,
+        "language": "Marathi"
+    },
+    {
+        "id": 4,
+        "language": "Gujarati"
+    },
+    {
+        "id": 5,
+        "language": "Tamil"
+    },
+    {
+        "id": 6,
+        "language": "Telugu"
+    },
+    {
+        "id": 7,
+        "language": "Kannada"
+    },
+    {
+        "id": 8,
+        "language": "Malayalam"
+    },
+    {
+        "id": 9,
+        "language": "French"
+    },
+    {
+        "id": 10,
+        "language": "German"
+    },
+    {
+        "id": 11,
+        "language": "Russian"
+    },
+    {
+        "id": 12,
+        "language": "Spanish"
+    },
+    {
+        "id": 13,
+        "language": "Mandarin Chinese"
+    },
+    {
+        "id": 14,
+        "language": "Punjabi"
+    },
+    {
+        "id": 15,
+        "language": "Assamese"
+    },
+    {
+        "id": 16,
+        "language": "Bengali"
+    },
+    {
+        "id": 17,
+        "language": "Oriya"
+    },
+    {
+        "id": 18,
+        "language": "Kashmiri"
+    },
+    {
+        "id": 19,
+        "language": "Konkani"
+    },
+    {
+        "id": 20,
+        "language": "Manipuri"
+    },
+    {
+        "id": 21,
+        "language": "Nepali"
+    },
+    {
+        "id": 22,
+        "language": "Sanskrit"
+    },
+    {
+        "id": 23,
+        "language": "Sindhi"
+    },
+    {
+        "id": 24,
+        "language": "Urdu"
+    },
+    {
+        "id": 25,
+        "language": "Bodo"
+    },
+    {
+        "id": 26,
+        "language": "Santhali"
+    },
+    {
+        "id": 27,
+        "language": "Maithili"
+    },
+    {
+        "id": 28,
+        "language": "Dogri"
+    },
+    {
+        "id": 29,
+        "language": "Angika"
+    },
+    {
+        "id": 30,
+        "language": "Banjara"
+    },
+    {
+        "id": 31,
+        "language": "Bazika"
+    },
+    {
+        "id": 32,
+        "language": "Bhojpuri"
+    },
+    {
+        "id": 33,
+        "language": "Bhoti"
+    },
+    {
+        "id": 34,
+        "language": "Bhotia"
+    },
+    {
+        "id": 35,
+        "language": "Bundelkhandi"
+    },
+    {
+        "id": 36,
+        "language": "Chhattisgarhi"
+    },
+    {
+        "id": 37,
+        "language": "Dhatki"
+    },
+    {
+        "id": 38,
+        "language": "Garhwali (Pahari)"
+    },
+    {
+        "id": 39,
+        "language": "Gondi"
+    },
+    {
+        "id": 40,
+        "language": "Gujjar\/Gujjari"
+    },
+    {
+        "id": 41,
+        "language": "Ho"
+    },
+    {
+        "id": 42,
+        "language": "Kachachhi"
+    },
+    {
+        "id": 43,
+        "language": "Kamtapuri"
+    },
+    {
+        "id": 44,
+        "language": "Karbi"
+    },
+    {
+        "id": 45,
+        "language": "Khasi"
+    },
+    {
+        "id": 46,
+        "language": "Kodava (Coorg)"
+    },
+    {
+        "id": 47,
+        "language": "Kok Barak"
+    },
+    {
+        "id": 48,
+        "language": "Kumaoni (Pahari)"
+    },
+    {
+        "id": 49,
+        "language": "Kurak"
+    },
+    {
+        "id": 50,
+        "language": "Kurmali"
+    },
+    {
+        "id": 51,
+        "language": "Lepcha"
+    },
+    {
+        "id": 52,
+        "language": "Limbu"
+    },
+    {
+        "id": 53,
+        "language": "Mizo (Lushai)"
+    },
+    {
+        "id": 54,
+        "language": "Magahi"
+    },
+    {
+        "id": 55,
+        "language": "Mundari"
+    },
+    {
+        "id": 56,
+        "language": "Nagpuri"
+    },
+    {
+        "id": 57,
+        "language": "Nicobarese"
+    },
+    {
+        "id": 58,
+        "language": "Himachali (Pahari)"
+    },
+    {
+        "id": 59,
+        "language": "Pali"
+    },
+    {
+        "id": 60,
+        "language": "Rajasthani (Marwadi)"
+    },
+    {
+        "id": 61,
+        "language": "Sambalpuri\/Kosali"
+    },
+    {
+        "id": 62,
+        "language": "Shaurseni (Prakrit)"
+    },
+    {
+        "id": 63,
+        "language": "Siraiki"
+    },
+    {
+        "id": 64,
+        "language": "Tenyidi"
+    },
+    {
+        "id": 65,
+        "language": "Tulu"
+    },
+    {
+        "id": 66,
+        "language": "Haryanvi"
+    },
+    {
+        "id": 67,
+        "language": "Arabic"
+    },
+    {
+        "id": 68,
+        "language": "Japanese"
+    },
+    {
+        "id": 69,
+        "language": "Tibetan"
+    },
+    {
+        "id": 70,
+        "language": "Korean"
+    },
+    {
+        "id": 71,
+        "language": "Malay"
+    },
+    {
+        "id": 72,
+        "language": "Portuguese"
+    },
+    {
+        "id": 73,
+        "language": "Swahili"
+    },
+    {
+        "id": 74,
+        "language": "Turkish"
+    },
+    {
+        "id": 75,
+        "language": "Italian"
+    },
+    {
+        "id": 76,
+        "language": "Persian"
+    },
+    {
+        "id": 77,
+        "language": "Thai"
+    },
+    {
+        "id": 78,
+        "language": "Vietnamese"
+    },
+    {
+        "id": 79,
+        "language": "Armenian"
+    },
+    {
+        "id": 80,
+        "language": "Burmese"
+    },
+    {
+        "id": 81,
+        "language": "Cantonese"
+    },
+    {
+        "id": 82,
+        "language": "Sinhala"
+    },
+    {
+        "id": 83,
+        "language": "Catalan"
+    },
+    {
+        "id": 84,
+        "language": "Danish"
+    },
+    {
+        "id": 85,
+        "language": "Dhivehi"
+    },
+    {
+        "id": 86,
+        "language": "Dutch"
+    },
+    {
+        "id": 87,
+        "language": "Finnish"
+    },
+    {
+        "id": 88,
+        "language": "Filipino"
+    },
+    {
+        "id": 89,
+        "language": "Fijian"
+    },
+    {
+        "id": 90,
+        "language": "Fula"
+    },
+    {
+        "id": 91,
+        "language": "Georgian"
+    },
+    {
+        "id": 92,
+        "language": "Indonesian"
+    },
+    {
+        "id": 93,
+        "language": "Polish"
+    },
+    {
+        "id": 94,
+        "language": "Latin"
+    },
+    {
+        "id": 95,
+        "language": "Swedish"
+    },
+    {
+        "id": 96,
+        "language": "Taiwanese"
+    },
+    {
+        "id": 97,
+        "language": "Venda"
+    },
+    {
+        "id": 98,
+        "language": "Ukrainian"
+    },
+    {
+        "id": 99,
+        "language": "Uzbek"
+    },
+    {
+        "id": 100,
+        "language": "Greek"
+    },
+    {
+        "id": 101,
+        "language": "Others"
+    }
+]
+
+export default language;
