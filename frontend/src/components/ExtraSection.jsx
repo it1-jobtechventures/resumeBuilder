45b284bfc48b64881502b5331aed757a0a9d46aa
it <it1.jobtechventure.com>
2025-03-31 18:02:57 +0530

    add useeffect

--- frontend/src/components/ExtraSection.jsx
+++ frontend/src/components/ExtraSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import ProjectSection from "./ProjectSection";
 import CertificationSection from "./CertificationSection";
 import HobbiesSection from "./HobbiesSection";
