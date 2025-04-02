b3d690d7bf645d696ed66f202643e8bef176187a
it <it1.jobtechventure.com>
2025-04-02 16:03:25 +0530

    check cookies is accepted or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import ResetPassword from './pages/ResetPassword'
 import ProfilePage from './pages/ProfilePage'
 import FeedbackForm from './components/FeedbackForm'
+import CookieConsent from './components/CookieConsent'
+import { useState } from 'react'
 
 function App() {
   const url = import.meta.env.VITE_BACKEND_URL;
+
+  const [cookieAccepted, setCookieAccepted] = useState(
+    localStorage.getItem("cookieConsent") === "true"
+  );
+
   return (
     <>
     <ToastContainer/>
+      {!cookieAccepted && <CookieConsent onAccept={() => setCookieAccepted(true)} />}
       <Routes>
         <Route path='/' element={<HomePage url={url}/>}/>
         <Route path='/login' element={<Login url={url}/>}/>
