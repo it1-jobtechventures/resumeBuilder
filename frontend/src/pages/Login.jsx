4b42dc9bab42b8b1fab7b4c333ac4d71ac8ea506
it <it1.jobtechventure.com>
2025-04-04 16:04:27 +0530

    change css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -46,10 +46,10 @@ const Login = () => {
     }
 
   return (
-    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 '>
         <img onClick={() => navigate('/')} src="/resumeRingerLogo.png" alt="" className='absolute left-5 sm:left-20 top-5 w-20 sm:w-20 cursor-pointer' />
-        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
-            <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
+        <div className='bg-gray-50 p-10 rounded-lg shadow-lg w-full sm:w-96     text-sm'>
+            <h2 className='text-3xl font-semibold text-black text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
             <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
             <form onSubmit={onSubmitHandler}>
                 {state === 'Sign Up' && (
@@ -58,16 +58,16 @@ const Login = () => {
                     <input onChange={e => setName(e.target.value)} value={name} type='text' placeholder='Full Name' className='bg-transparent outline-none' required />
                 </div>
             )}
-                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full border'>
                     <img src={mail_icon} alt="" />
                     <input onChange={e => setEmail(e.target.value)} value={email} type='email' placeholder='Email id' className='bg-transparent outline-none' required />
                 </div>
-                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full border text-black'>
                     <img src={lock_icon} alt="" />
-                    <input onChange={e => setPassword(e.target.value)} value={password} type='password' placeholder='Password' className='bg-transparent outline-none' required />
+                    <input onChange={e => setPassword(e.target.value)} value={password} type='password' placeholder='Password' className='bg-transparent outline-none text-black' required />
                 </div>
                 <p onClick={() => navigate('/reset-password')} className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password ?</p>
-                <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>{state}</button>
+                <button className='bg-[linear-gradient(90deg,_hsla(133,_68%,_60%,_1)_0%,_hsla(205,_97%,_42%,_1)_100%)] cursor-pointer text-white px-4 py-2 rounded-md hover:bg-[linear-gradient(90deg,_hsla(205,_97%,_42%,_1)_0%,_hsla(133,_68%,_60%,_1)_100%)] w-full'>{state}</button>
             </form>
             {state === "Sign Up" ? (
                 <p className='text-gray-400 text-center text-xs mt-4'>Already have an account ? {' '}
