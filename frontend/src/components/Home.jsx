b8b9c74aa948ca99adb679b73a4bec11d8f156a5
it <it1.jobtechventure.com>
2025-04-09 17:59:23 +0530

    created function to generate resumeId using api call save in local storage , use getprofile api to get is user id if user id logged in or use temporary userid to generate resume id

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import gif2 from '../assets/home.gif'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios';
 
-const Home = () => {
+const Home = ({url}) => {
+  const {backendurl , isLoggedIn , temporaryUserId} = useContext(AppContext)
+  const naviagte  = useNavigate()
+
+  const handleCreateResume = async () => {
+    let userId;
+    try {
+      if (isLoggedIn) {
+        console.log("c")
+        const res = await axios.get(`${url}/api/auth/profile` ,{ withCredentials: true,})
+        console.log(res.data)
+        console.log("home",res.data)
+        userId = res.data.user?._id
+        if (!userId) {
+          return res.status(400).json({ success: false, message: "User ID is missing" });
+      }
+        if (!userId) {
+          console.error("❌ User ID not found in profile response");
+          return;
+        }
+      }else{
+        userId = temporaryUserId
+        console.log("temporaryid from home", userId)
+      }
+
+      const response = await axios.post('http://localhost:5000/api/resume/create', {userId , isDraft:true})
+      // const resumeId = response.data.resumeId;
+      const resumeId = response.data._id;
+
+      console.log("resume id from home page" , resumeId)
+      localStorage.setItem('activeResumeId' , resumeId)
+      naviagte(`/createResume`)
+    } catch (error) {
+      console.error("Error creating resume:", error.response?.data || error.message);
+    }
+  }
 
   return (
     <>
@@ -15,7 +52,7 @@ const Home = () => {
             <p className='xl:text-2xl text-lg font-medium hidden md:block'>By employing the best practices and innovative tech, ResumeRinger boosts your chances of landing a better job – completely for free.</p>
           </section>
           <section className='  flex justify-left items-center gap-5'>
-            <Link to={'createResume'}><button className='cursor-pointer font-bold rounded-2xl bg-[#54df71] text-white p-2 text-center h-14 hover:border hover:border-[#54df71] hover:text-[#54df71] hover:bg-transparent'>Create Your Resume</button></Link>
+            <button onClick={handleCreateResume} className='cursor-pointer font-bold rounded-2xl bg-[#54df71] text-white p-2 text-center h-14 hover:border hover:border-[#54df71] hover:text-[#54df71] hover:bg-transparent'>Create Your Resume</button>
             {/* <button className='cursor-pointer font-bold rounded-2xl bg-[#037cd5] text-white p-2 text-center  h-14 hover:border hover:border-[#037cd5] hover:text-[#037cd5] hover:bg-transparent'>Import Your Resume</button>   */}
           </section>
         </section>
