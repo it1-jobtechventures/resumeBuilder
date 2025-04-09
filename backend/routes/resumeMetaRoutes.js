bdcf6c80dc3a2dab71ffb4d750ec5d850df17ac6
it <it1.jobtechventure.com>
2025-04-09 18:13:44 +0530

    update routes

--- backend/routes/resumeMetaRoutes.js
+++ backend/routes/resumeMetaRoutes.js
@@ -1,7 +1,26 @@
-import express from 'express'
-import { createResume, getUserResumes, updateResumeStatus } from '../controllers/resumeMetaController.js';
+// import express from 'express'
+// import { createResume, getUserResumes, updateResumeStatus } from '../controllers/resumeMetaController.js';
+// const resumeMetaRouter = express.Router();
+// resumeMetaRouter.post("/create", createResume);
+// resumeMetaRouter.get("/user/:userId", getUserResumes);
+// resumeMetaRouter.put("/update/:resumeId",updateResumeStatus);
+// export default resumeMetaRouter;
+
+import express from "express";
+import { createResumeMeta, getAllResumeMetas, getResumeMetaById, markResumeAsDownloaded} from "../controllers/resumeMetaController.js";
+
 const resumeMetaRouter = express.Router();
-resumeMetaRouter.post("/create", createResume);
-resumeMetaRouter.get("/user/:userId", getUserResumes);
-resumeMetaRouter.put("/update/:resumeId",updateResumeStatus);
+
+// Create a new resume meta
+resumeMetaRouter.post("/create", createResumeMeta);
+
+// Get all resume metas for a user
+resumeMetaRouter.get("/user/:userId", getAllResumeMetas);
+
+// Get a single resume meta by ID
+resumeMetaRouter.get("/:resumeMetaId", getResumeMetaById);
+
+// Mark a resume as downloaded
+resumeMetaRouter.patch("/download/:resumeMetaId", markResumeAsDownloaded);
+
 export default resumeMetaRouter;
