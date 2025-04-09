b87347fc687e84ccc03bc86a63210dfa2ec334d5
it <it1.jobtechventure.com>
2025-04-09 18:04:41 +0530

    change userID feild to get  temporary userID and already loggedin user id as a string

--- backend/model/resumeModel.js
+++ backend/model/resumeModel.js
@@ -1,23 +1,24 @@
 import mongoose from "mongoose";
-import interestsSectionModel from "./interestsSectionModel";
+
 
 const resumeSchema =  mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-  templateId: { type: mongoose.Schema.Types.ObjectId, ref: "template" },  // Selected template
+  // userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+ userId:{ type: String, required: true },
+  // templateId: { type: mongoose.Schema.Types.ObjectId, ref: "template" },  // Selected template
 
-  generalInfo: { type: mongoose.Schema.Types.ObjectId, ref: "generalinfo" },
-  workExperience: [{ type: mongoose.Schema.Types.ObjectId, ref: "workexperience" }],
+  generalInfo: { type: mongoose.Schema.Types.ObjectId, ref: "generalInfo" },
+  workExperience: [{ type: mongoose.Schema.Types.ObjectId, ref: "workExperience" }],
   education: [{ type: mongoose.Schema.Types.ObjectId, ref: "education" }],
-  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "project" }],
-  internships: [{ type: mongoose.Schema.Types.ObjectId, ref: "internshipexperience" }],
-  skills: [{ type: mongoose.Schema.Types.ObjectId, ref: "skill" }],
-  certifications: [{ type: mongoose.Schema.Types.ObjectId, ref: "certification" }],
-  languages: [{ type: mongoose.Schema.Types.ObjectId, ref: "language" }],
-  socialLinks: { type: mongoose.Schema.Types.ObjectId, ref: "sociallink" },
-  softwareInfo: [{ type: mongoose.Schema.Types.ObjectId, ref: "softwareinfo" }],
+  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "projects" }],
+  internshipExperience: [{ type: mongoose.Schema.Types.ObjectId, ref: "internshipExperience" }],
+  skill: [{ type: mongoose.Schema.Types.ObjectId, ref: "skill" }],
+  certification: [{ type: mongoose.Schema.Types.ObjectId, ref: "certification" }],
+  language: [{ type: mongoose.Schema.Types.ObjectId, ref: "language" }],
+  socialLinks: { type: mongoose.Schema.Types.ObjectId, ref: "socialLink" },
+  softwareInfo: [{ type: mongoose.Schema.Types.ObjectId, ref: "softwareInfo" }],
   reference: { type: mongoose.Schema.Types.ObjectId, ref: "reference" },
   accomplishment: { type: mongoose.Schema.Types.ObjectId, ref: "accomplishment" },
-  interestssection: { type: mongoose.Schema.Types.ObjectId, ref: "interestssection" },
+  interestsSection: { type: mongoose.Schema.Types.ObjectId, ref: "interestsSection" },
   volunteering: { type: mongoose.Schema.Types.ObjectId, ref: "volunteering" },
 
 
