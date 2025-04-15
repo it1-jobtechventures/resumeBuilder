759ee0a16da33ec030f533b6a86b060c75e5b0c6
it <it1.jobtechventure.com>
2025-04-15 17:40:39 +0530

    update the model

--- backend/model/internshipExperienceModel.js
+++ backend/model/internshipExperienceModel.js
@@ -5,17 +5,17 @@ const internshipExperienceSchema =  mongoose.Schema(
     // userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }, // Link to User
     userId:{type:String , required:true},
     resumeId: { type: mongoose.Schema.Types.ObjectId, ref: "resume", required: true }, // Link to Resume
-    company: { type: String, required: true },
-    location: { type: String, required: true },
-    title: { type: String, required: true },
-    startDate: { type: Date, required: true },
+    company: { type: String},
+    location: { type: String},
+    title: { type: String,},
+    startDate: { type: Date },
     endDate: { type: Date },
     currentlyWorking: { type: Boolean, default: false },
     description: { type: String },
     stipend: { type: String },
     noticePeriod: { type: String },
-    internshipType: { type: String, enum: [ "Full Time","Part Time","On Assignment","On Call","Freelancing","Specific Hours ","Work from Home / WFH"], required: true },
-    internshipMode: { type: String, enum: ["WFH", "Hybrid", "WFO"], required: true },
+    internshipType: { type: String, enum: [ "Full Time","Part Time","On Assignment","On Call","Freelancing","Specific Hours ","Work from Home / WFH"] },
+    internshipMode: { type: String, enum: ["WFH", "Hybrid", "WFO"]},
   },
   { timestamps: true }
 );
