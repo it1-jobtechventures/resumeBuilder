8d3bed3101ce0d00ff79ecd2595cae8fc83a868b
it <it1.jobtechventure.com>
2025-04-16 12:47:00 +0530

    addline

--- frontend/src/components/ResumeReview.jsx
+++ frontend/src/components/ResumeReview.jsx
@@ -690,6 +690,7 @@ const ResumeReview = ({ url }) => {
 
   const handleDownloadPDF = () => {
     const element = document.getElementById('resume-content');
+    element.style.width = '794px';
     const pdf = new jsPDF({ orientation: 'portrait', unit: 'mm', format: 'a4' });
 
     pdf.html(element, {
