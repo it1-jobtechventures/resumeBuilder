9d1f91408f3e3b48c5d9ea4a806b0f10d49c41ca
it <it1.jobtechventure.com>
2025-04-24 12:05:31 +0530

    remove userid

--- backend/model/skillsSectionModel.js
+++ backend/model/skillsSectionModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const skillsSchema =  mongoose.Schema(  {
   // userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }, // Link to User
-  userId:{type:String , required:true},
+  // userId:{type:String , required:true},
   resumeId: { type: mongoose.Schema.Types.ObjectId, ref: "resume", required: true }, // Link to Resume
   name: { type: String},
   level: { type: Number, min: 0}, // Skill proficiency percentage
