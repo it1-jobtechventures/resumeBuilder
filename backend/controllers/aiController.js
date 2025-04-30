18385189f1e07030a537ce227ccb070b2b97bea0
it <it1.jobtechventure.com>
2025-04-30 11:41:41 +0530

    created function for summary , job des and internship des

---
+++ backend/controllers/aiController.js
@@ -0,0 +1,53 @@
+import { generateSummary, generateWorkExperience } from "../model/aiModel.js";
+
+// Controller function to handle generating summary
+const aiGenerate = async (req, res) => {
+  const { designation, experience } = req.body;
+
+  if (!designation || !experience) {
+    return res.status(400).json({ error: 'Designation and experience are required' });
+  }
+
+  try {
+    // Call the model to generate the summary
+    const summary = await generateSummary(designation, experience);
+    return res.json({ summary });
+  } catch (error) {
+    console.error('Error generating summary:', error);
+    return res.status(500).json({ error: 'Failed to generate summary' });
+  }
+};
+
+const aiWorkExperience = async (req, res) => {
+  const { companyName, companyExperience, jobTitle } = req.body;
+  
+  if (!companyName || !companyExperience || !jobTitle) {
+    return res.status(400).json({ message: 'Missing required fields.' });
+  }
+  try {
+    // Call the model to generate the summary
+    const generatedDescription = await generateWorkExperience(companyName, companyExperience, jobTitle);
+    return res.json({ summary :generatedDescription});
+  } catch (error) {
+    console.error('Error generating summary:', error);
+    return res.status(500).json({ error: 'Failed to generate summary' });
+  }
+}
+
+const aiInternshipExperience = async (req, res) => {
+  const { company, title } = req.body;
+  
+  if (!company || !title ) {
+    return res.status(400).json({ message: 'Missing required fields.' });
+  }
+  try {
+    // Call the model to generate the summary
+    const generatedDescription = await generateWorkExperience(company, title);
+    return res.json({ summary :generatedDescription});
+  } catch (error) {
+    console.error('Error generating summary:', error);
+    return res.status(500).json({ error: 'Failed to generate summary' });
+  }
+}
+
+export {aiGenerate , aiWorkExperience,aiInternshipExperience}
