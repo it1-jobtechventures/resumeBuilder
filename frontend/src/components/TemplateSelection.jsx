01fc96cb3d5a01bd11904b16e6160e818683c18e
it <it1.jobtechventure.com>
2025-05-07 16:49:38 +0530

    add button to skip choosing template

--- frontend/src/components/TemplateSelection.jsx
+++ frontend/src/components/TemplateSelection.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import TemplateCard from './TemplateCard';
+import { useNavigate } from 'react-router-dom';
 
 const TemplateSelection = ({ templates }) => {
+  const navigate = useNavigate()
+
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
       <h2 className="text-2xl md:text-3xl font-bold text-center mb-8 text-gray-800">Choose Your Resume Template</h2>
@@ -9,6 +12,9 @@ const TemplateSelection = ({ templates }) => {
         {templates.map((template) => (
           <TemplateCard key={template._id} template={template} />
         ))}
+      </div>
+      <div>
+        <button onClick={() => navigate('/createResume')} className="mt-4 cursor-pointer bg-[linear-gradient(90deg,_hsla(133,_68%,_60%,_1)_0%,_hsla(205,_97%,_42%,_1)_100%)] cursor-pointer text-white px-4 py-2 rounded-md hover:bg-[linear-gradient(90deg,_hsla(205,_97%,_42%,_1)_0%,_hsla(133,_68%,_60%,_1)_100%)]">Choose later</button>
       </div>
     </div>
   );
